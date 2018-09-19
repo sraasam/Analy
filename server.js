2f6d16229a8fff5120e7bdc1ab67b6008c7a1894
Raasam <SRAASAM@ford.com>
2018-09-18 23:02:22 -0400

    changes

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var http = require('http');
+var express = require("express");
+var app = express();
+var port = 3000;
 
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!');
     
-}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 8080);*/
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public");
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
